ff61025dd56516179b4e3d0d647eb90bafbbcd16
gagral <keington@outlook.com>
2025-05-02 20:26:25 +0800

    feat: 增强 HPA 管理测试用例
    - 在 autoscale_controller_test.go 中添加针对带有 HPA 注解的 Deployment 的测试用例，包括创建、更新和删除 HPA 的逻辑。
    - 实现e2e测试逻辑

--- internal/controller/autoscale_controller_test.go
+++ internal/controller/autoscale_controller_test.go
@@ -17,16 +17,163 @@ limitations under the License.
 package controller
 
 import (
+	"context"
+	"time"
+
+	"github.com/infraflows/autoscale-controller/pkg/consts"
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
+	autoscalingv2 "k8s.io/api/autoscaling/v2"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 var _ = Describe("AutoScale Controller", func() {
-	Context("When reconciling a resource", func() {
+	const (
+		timeout  = time.Second * 10
+		interval = time.Millisecond * 250
+	)
 
-		It("should successfully reconcile the resource", func() {
+	Context("When reconciling a Deployment with HPA annotations", func() {
+		var (
+			ctx            context.Context
+			deployment     *appsv1.Deployment
+			namespace      string
+			deploymentName string
+		)
 
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		BeforeEach(func() {
+			ctx = context.Background()
+			namespace = "test-namespace"
+			deploymentName = "test-deployment"
+
+			// 创建测试命名空间
+			ns := &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: namespace,
+				},
+			}
+			Expect(k8sClient.Create(ctx, ns)).Should(Succeed())
+
+			// 创建测试 Deployment
+			deployment = &appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      deploymentName,
+					Namespace: namespace,
+					Annotations: map[string]string{
+						consts.HPAMinReplicas:                 "2",
+						consts.HPAMaxReplicas:                 "10",
+						consts.HPACpuTargetAverageUtilization: "80",
+					},
+				},
+				Spec: appsv1.DeploymentSpec{
+					Selector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"app": deploymentName,
+						},
+					},
+					Template: corev1.PodTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Labels: map[string]string{
+								"app": deploymentName,
+							},
+						},
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "nginx",
+									Image: "nginx:1.14.2",
+								},
+							},
+						},
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, deployment)).Should(Succeed())
+		})
+
+		AfterEach(func() {
+			// 清理测试资源
+			Expect(k8sClient.Delete(ctx, deployment)).Should(Succeed())
+			Expect(k8sClient.Delete(ctx, &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}})).Should(Succeed())
+		})
+
+		It("Should create HPA when deployment has HPA annotations", func() {
+			// 等待 HPA 创建
+			Eventually(func() error {
+				hpa := &autoscalingv2.HorizontalPodAutoscaler{}
+				return k8sClient.Get(ctx, types.NamespacedName{
+					Name:      deploymentName,
+					Namespace: namespace,
+				}, hpa)
+			}, timeout, interval).Should(Succeed())
+
+			// 验证 HPA 配置
+			hpa := &autoscalingv2.HorizontalPodAutoscaler{}
+			Expect(k8sClient.Get(ctx, types.NamespacedName{
+				Name:      deploymentName,
+				Namespace: namespace,
+			}, hpa)).Should(Succeed())
+
+			Expect(hpa.Spec.MinReplicas).Should(HaveValue(Equal(int32(2))))
+			Expect(hpa.Spec.MaxReplicas).Should(Equal(int32(10)))
+			Expect(hpa.Spec.Metrics).Should(HaveLen(1))
+			Expect(hpa.Spec.Metrics[0].Resource.Name).Should(Equal(corev1.ResourceCPU))
+			Expect(hpa.Spec.Metrics[0].Resource.Target.AverageUtilization).Should(HaveValue(Equal(int32(80))))
+		})
+
+		It("Should update HPA when deployment annotations change", func() {
+			// 更新 Deployment 注解
+			Eventually(func() error {
+				if err := k8sClient.Get(ctx, types.NamespacedName{
+					Name:      deploymentName,
+					Namespace: namespace,
+				}, deployment); err != nil {
+					return err
+				}
+				deployment.Annotations[consts.HPAMaxReplicas] = "15"
+				return k8sClient.Update(ctx, deployment)
+			}, timeout, interval).Should(Succeed())
+
+			// 验证 HPA 更新
+			Eventually(func() int32 {
+				hpa := &autoscalingv2.HorizontalPodAutoscaler{}
+				if err := k8sClient.Get(ctx, types.NamespacedName{
+					Name:      deploymentName,
+					Namespace: namespace,
+				}, hpa); err != nil {
+					return 0
+				}
+				return hpa.Spec.MaxReplicas
+			}, timeout, interval).Should(Equal(int32(15)))
+		})
+
+		It("Should delete HPA when deployment annotations are removed", func() {
+			// 移除 HPA 注解
+			Eventually(func() error {
+				if err := k8sClient.Get(ctx, types.NamespacedName{
+					Name:      deploymentName,
+					Namespace: namespace,
+				}, deployment); err != nil {
+					return err
+				}
+				delete(deployment.Annotations, consts.HPAMinReplicas)
+				delete(deployment.Annotations, consts.HPAMaxReplicas)
+				delete(deployment.Annotations, consts.HPACpuTargetAverageUtilization)
+				return k8sClient.Update(ctx, deployment)
+			}, timeout, interval).Should(Succeed())
+
+			// 验证 HPA 被删除
+			Eventually(func() bool {
+				hpa := &autoscalingv2.HorizontalPodAutoscaler{}
+				err := k8sClient.Get(ctx, types.NamespacedName{
+					Name:      deploymentName,
+					Namespace: namespace,
+				}, hpa)
+				return err != nil
+			}, timeout, interval).Should(BeTrue())
 		})
 	})
 })
--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -19,15 +19,25 @@ package e2e
 import (
 	"fmt"
 	"os/exec"
+	"strconv"
+	"strings"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	"github.com/infraflows/autoscale-controller/pkg/consts"
 	"github.com/infraflows/autoscale-controller/test/utils"
+	"gopkg.in/yaml.v3"
+	appsv1 "k8s.io/api/apps/v1"
+	autoscalingv2 "k8s.io/api/autoscaling/v2"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const namespace = "autoscale-controller-system"
+const testNamespace = "test-namespace"
 
 var _ = Describe("controller", Ordered, func() {
 	BeforeAll(func() {
@@ -40,6 +50,10 @@ var _ = Describe("controller", Ordered, func() {
 		By("creating manager namespace")
 		cmd := exec.Command("kubectl", "create", "ns", namespace)
 		_, _ = utils.Run(cmd)
+
+		By("creating test namespace")
+		cmd = exec.Command("kubectl", "create", "ns", testNamespace)
+		_, _ = utils.Run(cmd)
 	})
 
 	AfterAll(func() {
@@ -51,6 +65,10 @@ var _ = Describe("controller", Ordered, func() {
 
 		By("removing manager namespace")
 		cmd := exec.Command("kubectl", "delete", "ns", namespace)
+		_, _ = utils.Run(cmd)
+
+		By("removing test namespace")
+		cmd = exec.Command("kubectl", "delete", "ns", testNamespace)
 		_, _ = utils.Run(cmd)
 	})
 
@@ -84,7 +102,6 @@ var _ = Describe("controller", Ordered, func() {
 			By("validating that the controller-manager pod is running as expected")
 			verifyControllerUp := func() error {
 				// Get pod name
-
 				cmd = exec.Command("kubectl", "get",
 					"pods", "-l", "control-plane=controller-manager",
 					"-o", "go-template={{ range .items }}"+
@@ -116,7 +133,123 @@ var _ = Describe("controller", Ordered, func() {
 				return nil
 			}
 			EventuallyWithOffset(1, verifyControllerUp, time.Minute, time.Second).Should(Succeed())
+		})
 
+		It("should create HPA for deployment with annotations", func() {
+			By("creating a deployment with HPA annotations")
+			deployment := &appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-deployment",
+					Namespace: testNamespace,
+					Annotations: map[string]string{
+						consts.HPAMinReplicas:                 "2",
+						consts.HPAMaxReplicas:                 "10",
+						consts.HPACpuTargetAverageUtilization: "80",
+					},
+				},
+				Spec: appsv1.DeploymentSpec{
+					Selector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"app": "test-app",
+						},
+					},
+					Template: corev1.PodTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Labels: map[string]string{
+								"app": "test-app",
+							},
+						},
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "nginx",
+									Image: "nginx:1.14.2",
+									Resources: corev1.ResourceRequirements{
+										Requests: corev1.ResourceList{
+											corev1.ResourceCPU:    resource.MustParse("100m"),
+											corev1.ResourceMemory: resource.MustParse("128Mi"),
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+
+			cmd := exec.Command("kubectl", "apply", "-f", "-")
+			cmd.Stdin = strings.NewReader(ToYAML(deployment))
+			_, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("verifying HPA is created")
+			Eventually(func() error {
+				cmd = exec.Command("kubectl", "get", "hpa", "test-deployment", "-n", testNamespace)
+				_, err := utils.Run(cmd)
+				return err
+			}, time.Minute, time.Second).Should(Succeed())
+
+			By("verifying HPA configuration")
+			cmd = exec.Command("kubectl", "get", "hpa", "test-deployment", "-n", testNamespace, "-o", "yaml")
+			output, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+
+			hpa := &autoscalingv2.HorizontalPodAutoscaler{}
+			err = yaml.Unmarshal(output, hpa)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(*hpa.Spec.MinReplicas).To(Equal(int32(2)))
+			Expect(hpa.Spec.MaxReplicas).To(Equal(int32(10)))
+			Expect(hpa.Spec.Metrics).To(HaveLen(1))
+			Expect(hpa.Spec.Metrics[0].Resource.Name).To(Equal(corev1.ResourceCPU))
+			Expect(*hpa.Spec.Metrics[0].Resource.Target.AverageUtilization).To(Equal(int32(80)))
+		})
+
+		It("should update HPA when deployment annotations change", func() {
+			By("updating deployment annotations")
+			cmd := exec.Command("kubectl", "patch", "deployment", "test-deployment", "-n", testNamespace,
+				"--type=json", "-p", `[{"op": "replace", "path": "/metadata/annotations/hpa.infraflow.co~1maxReplicas", "value": "15"}]`)
+			_, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("verifying HPA is updated")
+			Eventually(func() int32 {
+				cmd = exec.Command("kubectl", "get", "hpa", "test-deployment", "-n", testNamespace, "-o", "jsonpath={.spec.maxReplicas}")
+				output, err := utils.Run(cmd)
+				if err != nil {
+					return 0
+				}
+				maxReplicas, _ := strconv.ParseInt(string(output), 10, 32)
+				return int32(maxReplicas)
+			}, time.Minute, time.Second).Should(Equal(int32(15)))
+		})
+
+		It("should delete HPA when deployment annotations are removed", func() {
+			By("removing HPA annotations")
+			cmd := exec.Command("kubectl", "patch", "deployment", "test-deployment", "-n", testNamespace,
+				"--type=json", "-p", `[
+					{"op": "remove", "path": "/metadata/annotations/hpa.infraflow.co~1minReplicas"},
+					{"op": "remove", "path": "/metadata/annotations/hpa.infraflow.co~1maxReplicas"},
+					{"op": "remove", "path": "/metadata/annotations/hpa.infraflow.co~1cpu.targetAverageUtilization"}
+				]`)
+			_, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("verifying HPA is deleted")
+			Eventually(func() error {
+				cmd = exec.Command("kubectl", "get", "hpa", "test-deployment", "-n", testNamespace)
+				_, err := utils.Run(cmd)
+				return err
+			}, time.Minute, time.Second).Should(HaveOccurred())
 		})
 	})
 })
+
+// ToYAML converts an object to YAML format
+func ToYAML(obj interface{}) string {
+	data, err := yaml.Marshal(obj)
+	if err != nil {
+		return ""
+	}
+	return string(data)
+}
